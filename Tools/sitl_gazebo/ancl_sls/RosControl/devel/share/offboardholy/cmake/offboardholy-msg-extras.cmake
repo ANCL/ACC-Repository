92f238dafbfb551fe040188669737fae4354129e
Edward Yan <edward929.yan@gmail.com>
2024-07-24 15:06:59 -0600

    submodule for multifile

--- Tools/sitl_gazebo/ancl_sls/RosControl/devel/share/offboardholy/cmake/offboardholy-msg-extras.cmake
+++ Tools/sitl_gazebo/ancl_sls/RosControl/devel/share/offboardholy/cmake/offboardholy-msg-extras.cmake
@@ -1,2 +1,2 @@
-set(offboardholy_MESSAGE_FILES "/home/ejyan/ACC-Repository/Tools/sitl_gazebo/ancl_sls/RosControl/src/offboardholy/msg/PTStates.msg;/home/ejyan/ACC-Repository/Tools/sitl_gazebo/ancl_sls/RosControl/src/offboardholy/msg/ActuatorControl0.msg;/home/ejyan/ACC-Repository/Tools/sitl_gazebo/ancl_sls/RosControl/src/offboardholy/msg/AttOut.msg")
+set(offboardholy_MESSAGE_FILES "/home/ejyan/ACC-Repository/Tools/sitl_gazebo/ancl_sls/RosControl/src/Multi-File/msg/PTStates.msg;/home/ejyan/ACC-Repository/Tools/sitl_gazebo/ancl_sls/RosControl/src/Multi-File/msg/ActuatorControl0.msg;/home/ejyan/ACC-Repository/Tools/sitl_gazebo/ancl_sls/RosControl/src/Multi-File/msg/AttOut.msg")
 set(offboardholy_SERVICE_FILES "")
